fc6cc5ada1a7e44d0ad3b82f7aeb740151a0fdb8
mads1539 <mads1539@gmail.com>
2026-03-25 22:16:56 +0100

    0.01261 bugfix

--- src/data/airlines/index.js
+++ src/data/airlines/index.js
@@ -2,7 +2,7 @@ import { CITIES } from './cities.js';
 import { TRANSIT_LINES } from './transitlines.js';
 import { CATEGORY_STYLE } from './styles.js';
 
-export const denmarkData = {
+export const airData = {
     cities: CITIES,
     lines: TRANSIT_LINES,
     styles: CATEGORY_STYLE,
